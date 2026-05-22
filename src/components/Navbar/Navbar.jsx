58f6105e7ae2b39fec19ce0f9cf06f4d7b320366
hashem03x <hhashemaahmed@gmail.com>
2026-05-22 17:15:54 +0300

    enhancements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -66,14 +66,14 @@ export default function Navbar() {
   const handleScrollOrRoute = (scrollTarget) => {
     if (location.pathname === "/" || location.pathname === "") {
       animateScroll.scrollTo(
-        document.getElementById(scrollTarget).offsetTop - 70
+        document.getElementById(scrollTarget).offsetTop - 70,
       );
       ScrollLink.scrollTo(scrollTarget);
     } else {
       navigate("/");
       setTimeout(() => {
         animateScroll.scrollTo(
-          document.getElementById(scrollTarget).offsetTop - 70
+          document.getElementById(scrollTarget).offsetTop - 70,
         );
         ScrollLink.scrollTo(scrollTarget);
       }, 50);
@@ -250,6 +250,15 @@ export default function Navbar() {
               </ScrollLink>
             </li>
             <li>
+              <NavLink
+                to="privacyPolicy"
+                className="text-white cursor-pointer"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Privacy Policy
+              </NavLink>
+            </li>
+            <li>
               {isLoggedIn ? (
                 <NavLink
                   to="manageplaylist"
@@ -268,8 +277,20 @@ export default function Navbar() {
                   }}
                   title={langValue["Login"]}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="22" height="22">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    width="22"
+                    height="22"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                    />
                   </svg>
                 </NavLink>
               )}
